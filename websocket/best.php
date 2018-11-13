5a3c80f10ec34906408b1c7c1a95eaf31c85a686
supermopf <victor.lange1998@gmail.com>
2018-11-13 17:08:04 +0100

    Changed to HTTPS

--- websocket/best.php
+++ websocket/best.php
@@ -18,8 +18,8 @@ if (isset($_GET["action"]) && $_GET["action"] == "delete") {
     <title>IT Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
     <!-- CSS Libs -->
     <link rel="stylesheet" type="text/css" href="../monitor/lib/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../monitor/lib/css/font-awesome.min.css">
--- websocket/index.php
+++ websocket/index.php
@@ -5,8 +5,8 @@
     <title>IT Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
     <!-- CSS Libs -->
     <link rel="stylesheet" type="text/css" href="../monitor/lib/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../monitor/lib/css/font-awesome.min.css">
