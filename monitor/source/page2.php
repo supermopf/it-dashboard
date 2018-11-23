1dd515146e85c2713a934589d229b4ee9552fa16
supermopf <victor.lange1998@gmail.com>
2018-11-23 13:43:38 +0100

    Chart for Tickets

--- monitor/source/page2.php
+++ monitor/source/page2.php
@@ -62,68 +62,53 @@ if ($conn) {
         die(print_r(sqlsrv_errors(), true));
     }
 
+    if ($result = sqlsrv_query($conn,"
+        SELECT Count([referencenumber]) As NumberOfTickets, DATEPART(HOUR, [registrationtime]) AS Hour
+        FROM [HL_Data].[dbo].[SBl_SSa_IncRecSerReq_kw]
+        WHERE referencenumber LIKE FORMAT(GETDATE(), 'yyyyMMdd')+'%' 
+        AND [keyword] NOT LIKE 'zz_%'  
+        AND [keyword] NOT LIKE 'ERP%'
+        AND [keyword] NOT LIKE 'Programmierung%'
+        AND [keyword] NOT LIKE 'PDM%'
+        AND [keyword] NOT LIKE 'Facili%'
+        AND [keyword] NOT LIKE 'Mobi%'
+        AND [keyword] NOT LIKE 'OMS%'
+        AND [keyword] NOT LIKE 'ECM%'
+        AND [keyword] NOT LIKE 'BI%'
+        AND [keyword] NOT LIKE 'DPL%'
+        AND [keyword] NOT LIKE 'EDI%'
+        AND [keyword] NOT LIKE 'CST%'
+        
+        GROUP BY DATEPART(HOUR, [registrationtime])"
+    )) {
+        $TicketChart = array();
+        while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+            $TicketChart[$row['Hour']] = $row['NumberOfTickets'];
+        }
+    } else {
+        echo "<pre>";
+        die(print_r(sqlsrv_errors(), true));
+    }
 
 
-    if ($result = sqlsrv_query($conn, "SELECT TOP 10 * FROM [HL_Data].[dbo].[SBl_SSa_IncRecSerReq_kw]
-      WHERE [state] IN ('Offen', 'Zu prüfen') AND ((
-             reservedby IN ('CHRISTIAN FORNER',
-                                  'STEFAN AMTMANN',
-                                  'ARIAN AMIR',
-                                  'ADAM ATAK',
-                                  'HEIKO LABENS',
-                                  'ABOUDOU ROSENBERG',
-                                  'THOMAS KORTE',
-                                  'VICTOR LANGE',
-                                  'STEVEN SACHERT',
-                                  'JULIAN SIEBERT',
-                                  'GERRIT GROTE',
-                                  'SEBASTIAN NITZ',
-                                  'FLORIAN SIBUS',
-                                  'MICHAEL PTASCHINSKI',
-                                  'STEFAN LÜKE',
-                                  'FABIAN FISCHER',
-                                  'PATRICK PUSCH',
-                                  'MICHAEL RADEMACHER')
-             ) OR (
-             assignedagent IN ('CHRISTIANFORNER',
-                                        'STEFANAMTMANN',
-                                        'ARIANAMIR',
-                                        'ADAMATAK',
-                                        'HEIKOLABENS',
-                                        'ABOUDOUROSENBERG',
-                                        'THOMASKORTE',
-                                        'VICTORLANGE',
-                                        'STEVENSACHERT',
-                                        'JULIANSIEBERT',
-                                        'GERRITGROTE',
-                                        'SEBASTIANNITZ',
-                                        'FLORIANSIBUS',
-                                        'MICHAELPTASCHINSKI',
-                                        'STEFANLUEKE',
-                                        'FABIANFISCHER',
-                                        'PATRICKPUSCH',
-                                        'MICHAELRADEMACHER')
-             ) OR (
-                    [keyword] NOT LIKE 'zz_%'  
-                    AND [keyword] NOT LIKE 'ERP%'
-            AND [keyword] NOT LIKE 'Programmierung%'
-            AND [keyword] NOT LIKE 'PDM%'
-            AND [keyword] NOT LIKE 'Facili%'
-            AND [keyword] NOT LIKE 'Mobi%'
-            AND [keyword] NOT LIKE 'OMS%'
-            AND [keyword] NOT LIKE 'ECM%'
-            AND [keyword] NOT LIKE 'BI%'
-            AND [keyword] NOT LIKE 'DPL%'
-            AND [keyword] NOT LIKE 'EDI%'
-                    AND [priority] IN ('TOP', 'Hoch')
-             ) )
-             AND [promisedsolutiontime] IS NOT NULL
-     ORDER BY case when [priority] = 'TOP' then 1
-                                  when [priority] = 'Hoch' then 2
-                                  when [priority] = 'Mittel' then 3
-                                  when [priority] = 'Niedrig' then 4
-                                  else 5
-                           end ASC, [promisedsolutiontime] ASC
+
+    if ($result = sqlsrv_query($conn, "
+        SELECT *
+        FROM [HL_Data].[dbo].[SBl_SSa_IncRecSerReq_kw]
+        WHERE referencenumber LIKE FORMAT(GETDATE(), 'yyyyMMdd')+'%' 
+        AND [keyword] NOT LIKE 'zz_%'  
+        AND [keyword] NOT LIKE 'ERP%'
+        AND [keyword] NOT LIKE 'Programmierung%'
+        AND [keyword] NOT LIKE 'PDM%'
+        AND [keyword] NOT LIKE 'Facili%'
+        AND [keyword] NOT LIKE 'Mobi%'
+        AND [keyword] NOT LIKE 'OMS%'
+        AND [keyword] NOT LIKE 'ECM%'
+        AND [keyword] NOT LIKE 'BI%'
+        AND [keyword] NOT LIKE 'DPL%'
+        AND [keyword] NOT LIKE 'EDI%'
+        AND [keyword] NOT LIKE 'CST%'
+        ORDER BY [registrationtime]
 ")) {
         $array = array();
         while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
@@ -161,98 +146,111 @@ if ($conn) {
             </div>
         </div>
     </div>
+</div>
 <div class="row">
-    <div class="col-lg-12">
-        <!-- Table -->
-        <table class="table table-responsive table-striped">
-            <thead>
-            <tr>
-                <th width="10%" class="h4">Ticketnummer</th>
-                <th width="20%" class="h4">Betreff</th>
-                <th width="15%" class="h4">Anfrager</th>
-                <th width="15%" class="h4">Keyword</th>
-                <th width="5%" class="h4">Priorität</th>
-                <th width="10%" class="h4">Zuständiger Agent</th>
-                <th width="10%" class="h4">Reserviert von</th>
-                <th width="10%" class="h4">Versprochende Lösungszeit</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $time = new DateTime();
-            $prio = array(
-                "TOP" => "<strong>TOP</strong>",
-                "Hoch" => "<strong>Hoch</strong>",
-                "Mittel" => "Mittel",
-                "Niedrig" => "Niedrig"
-            );
-            foreach ($array as $key => $Value) {
-                $dateObj = $array[$key]["promisedsolutiontime"];
-                if ($dateObj instanceof \DateTime) {
-                    $diff = $time->diff($dateObj);
-                }
-                $color = "";
-                $prefix = "";
-                if ($diff->format("%r%a") <= 0) {
-                    $color = "danger";
-                    $prefix = "Vor ";
-                } else {
-                    $prefix = "In ";
-                }
-                if ($diff->format("%r%a") <= 0) {
-                    $color = "danger";
-                    $prefix = "Vor ";
-                }
-                $datestr = "";
-                if ($diff->format("%a") >= 365) {
-                    if ($diff->format("%Y") != 1) {
-                        $datestr = "%Y Jahren";
-                    } else {
-                        $datestr = "%y Jahr";
-                    }
-                } elseif ($diff->format("%a") >= 31) {
-                    if ($diff->format("%m") > 1) {
-                        $datestr = "%m Monaten";
-                    } else {
-                        $datestr = "%m Monat";
-                    }
-                } elseif ($diff->format("%d") >= 1) {
-                    if ($diff->format("%d") != 1) {
-                        $datestr = "%d Tagen";
-                    } else {
-                        $datestr = "%d Tag";
-                    }
-                } elseif ($diff->format("%h") <= 24) {
-                    if ($diff->format("%h") != 1) {
-                        $datestr = "%H Stunden";
-                    } else {
-                        $datestr = "%h Stunde";
-                    }
-                } else {
-                    if ($diff->format("%i") != 1) {
-                        $datestr = "%I Minuten";
-                    } else {
-                        $datestr = "%i Minute";
-                    }
-                }
-
-                $assignedagent = preg_replace('/(?<!\ )[A-Z]/', ' $0', $array[$key]["assignedagent"]);
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title">
+                    <div class="title">Heutiges Ticketaufkommen</div>
+                </div>
+            </div>
+            <div class="card-body no-padding">
+                <canvas id="TicketChart" class="chart" width="478" height="260" style="width: 478px; height: 260px;"></canvas>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="card summary-inline">
+            <div class="card-body">
+                <!-- Table -->
+                <table class="table table-responsive table-striped">
+                    <thead>
+                    <tr>
+                        <th style="width: 10%;" class="h4">Ticketnummer</th>
+                        <th class="h4">Betreff</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $time = new DateTime();
+                    foreach ($array as $key => $Value) {
+                        $dateObj = $array[$key]["promisedsolutiontime"];
+                        $color = "";
+                        $assignedagent = preg_replace('/(?<!\ )[A-Z]/', ' $0', $array[$key]["assignedagent"]);
 
 
-                echo "<tr class='helplinetable'>";
-                echo "<td class='$color h4 text'><span>" . $array[$key]["referencenumber"] . "</span></td>";
-                echo "<td class='$color h4 text'><span>" . $array[$key]["subject"] . "</span></td>";
-                echo "<td class='$color h4 text'><span>" . $array[$key]["requestername"] . "</span></td>";
-                echo "<td class='$color h4 text'><span>" . $array[$key]["keyword"] . "</span></td>";
-                echo "<td class='$color h4 text'><span>" . $prio[$array[$key]["priority"]] . "</span></td>";
-                echo "<td class='$color h4 text'><span>" . $assignedagent . "</span></td>";
-                echo "<td class='$color h4 text'><span>" . $array[$key]["reservedby"] . "</span></td>";
-                echo "<td class='$color h4 text'><span>" . $diff->format($prefix . $datestr) . "</span></td>";
-                echo "</tr>";
-            }
-            ?>
-            </tbody>
-        </table>
+                        echo "<tr class='helplinetable'>";
+                        echo "<td class='$color h4 text'><span>" . $array[$key]["referencenumber"] . "</span></td>";
+                        echo "<td class='$color h4 text'><span>" . $array[$key]["subject"] . "</span></td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 
+<?php
+$ChartLabels = array();
+$ChartData = array();
+for($i = 0; $i <= 23; $i++){
+    $time = strtotime($i . ':00:00');
+    if(isset($TicketChart[$i])){
+        array_push($ChartLabels,date("G:i",$time));
+        array_push($ChartData, $TicketChart[$i]);
+    }else{
+        array_push($ChartLabels, date("G:i",$time));
+        array_push($ChartData, 0);
+    }
+}
+$ChartLabels = json_encode($ChartLabels);
+$ChartData = json_encode($ChartData);
+?>
+
+<script>
+var options = {
+    scales: {
+        xAxes: [{
+            ticks: {
+                fontSize: 15
+            }
+        }],
+        yAxes: [{
+            ticks: {
+                min: 0,
+                max: 10,
+                fontSize: 15
+            }
+        }]
+    },
+    elements: {
+        line: {
+            tension: 0.3
+        }
+    },
+    borderCapStyle: 'butt'
+};
+var data = {
+        labels: <?php echo $ChartLabels ?>,
+        datasets: [{
+        label: 'Anzahl Tickets',
+        data: <?php echo $ChartData ?>,
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+        ],
+        borderWidth: 1
+    }]
+};
+ctx = $('#TicketChart').get(0).getContext('2d');
+var TicketChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options
+});
+</script>
