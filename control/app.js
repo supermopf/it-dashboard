480ecb91bff6f9bae3fab87969f4824d1efedfae
supermopf <victor.lange1998@gmail.com>
2018-12-11 11:33:02 +0100

    Bugfix

--- control/app.js
+++ control/app.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     // Populate dropdown with list of provinces
     $.getJSON(url, function (data) {
         $.each(data, function (key, entry) {
-            ImageMatch[entry.url] = entry["RadioImage"];
+            ImageMatch[entry["RadioURL"]] = entry["RadioImage"];
             dropdown.append($('<option></option>').attr('value', entry["RadioURL"]).text(entry["RadioName"]));
         })
     });
